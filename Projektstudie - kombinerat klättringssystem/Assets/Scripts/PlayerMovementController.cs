5748e77fa592878af64d2d145b525d2d750a2899
Zenzyde <embi4882@gmail.com>
2020-08-09 10:49:57 +0200

    Separated angle-detection
    Separated angle-detection into outer-detection & inner-detection for separate wall-climbing thresholds.

--- Projektstudie - kombinerat klättringssystem/Assets/Scripts/PlayerMovementController.cs
+++ Projektstudie - kombinerat klättringssystem/Assets/Scripts/PlayerMovementController.cs
@@ -5,72 +5,72 @@ using Cinemachine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    [SerializeField] private Vector3 m_moveSpeed;
-    [SerializeField] private float m_maxLerpCheck, m_maxClimbEnterAngle, m_maxClimbAroundWallAngle, m_normalOffset = 0.2f;
-    [SerializeField] private Transform m_climbingCamOffset, m_jumpVisual;
-    [SerializeField] private CinemachineFreeLook m_freelookCam;
+	[SerializeField] private Vector3 m_moveSpeed;
+	[SerializeField] private float m_maxLerpCheck, m_maxClimbEnterAngle, m_maxClimbAroundWallOuterAngle, m_maxClimbAroundWallInnerAngle, m_normalOffset = 0.2f;
+	[SerializeField] private Transform m_climbingCamOffset, m_jumpVisual;
+	[SerializeField] private CinemachineFreeLook m_freelookCam;
 
-    private Camera m_cam;
-    private Vector3 move;
+	private Camera m_cam;
+	private Vector3 move;
 
-    public PlayerStateInterface m_currentState;
-    public PlayerWalking walkingState;
-    public PlayerClimbing climbingState;
+	public PlayerStateInterface m_currentState;
+	public PlayerWalking walkingState;
+	public PlayerClimbingNew climbingState;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_cam = Camera.main;
+	// Start is called before the first frame update
+	void Start()
+	{
+		m_cam = Camera.main;
 
-        walkingState = new PlayerWalking(this, transform, m_moveSpeed, m_cam, m_maxClimbEnterAngle, GetComponent<Rigidbody>(), m_normalOffset);
-        climbingState = new PlayerClimbing(this, transform, m_moveSpeed, m_cam, m_maxLerpCheck, GetComponent<Rigidbody>(), m_jumpVisual, m_maxClimbEnterAngle, m_normalOffset,
-                            m_maxClimbAroundWallAngle);
-        m_currentState = walkingState;
-        m_currentState.Init();
-    }
+		walkingState = new PlayerWalking(this, transform, m_moveSpeed, m_cam, m_maxClimbEnterAngle, GetComponent<Rigidbody>(), m_normalOffset);
+		climbingState = new PlayerClimbingNew(this, transform, m_moveSpeed, m_cam, m_maxLerpCheck, GetComponent<Rigidbody>(), m_jumpVisual, m_maxClimbEnterAngle, m_normalOffset,
+							m_maxClimbAroundWallOuterAngle, m_maxClimbAroundWallInnerAngle, m_freelookCam);
+		m_currentState = walkingState;
+		m_currentState.Init();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        m_currentState.Update();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		m_currentState.Update();
+	}
 
-    void FixedUpdate()
-    {
-        m_currentState.FixedUpdate();
-    }
+	void FixedUpdate()
+	{
+		m_currentState.FixedUpdate();
+	}
 
-    void OnCollisionEnter(Collision hit)
-    {
-        m_currentState.OnCollisionEnter(hit);
-    }
+	void OnCollisionEnter(Collision hit)
+	{
+		m_currentState.OnCollisionEnter(hit);
+	}
 
-    void OnCollisionStay(Collision hit)
-    {
-        m_currentState.OnCollisionStay(hit);
-    }
+	void OnCollisionStay(Collision hit)
+	{
+		m_currentState.OnCollisionStay(hit);
+	}
 
-    void OnCollisionExit(Collision hit)
-    {
-        m_currentState.OnCollisionExit(hit);
-    }
+	void OnCollisionExit(Collision hit)
+	{
+		m_currentState.OnCollisionExit(hit);
+	}
 
-    void OnDrawGizmos()
-    {
-        m_currentState.OnDrawGizmos();
-    }
+	void OnDrawGizmos()
+	{
+		m_currentState.OnDrawGizmos();
+	}
 
-    public void SetCamState(PlayerStateInterface state)
-    {
-        if (state == climbingState)
-        {
-            m_freelookCam.LookAt = m_climbingCamOffset;
-            m_freelookCam.Follow = m_climbingCamOffset;
-        }
-        else
-        {
-            m_freelookCam.LookAt = transform;
-            m_freelookCam.Follow = transform;
-        }
-    }
+	public void SetCamState(PlayerStateInterface state)
+	{
+		if (state == climbingState)
+		{
+			m_freelookCam.LookAt = m_climbingCamOffset;
+			m_freelookCam.Follow = m_climbingCamOffset;
+		}
+		else
+		{
+			m_freelookCam.LookAt = transform;
+			m_freelookCam.Follow = transform;
+		}
+	}
 }
